7cb15702426ef4613ad58bf590e879437ead5feb
Ego  git config --global user.email <you@example.com>
2023-05-30 15:28:19 +0300

    start

---
+++ interface.py
@@ -0,0 +1,11 @@
+def start ():
+    path = 'phone_book.txt'
+
+    try:
+        file = open(path, 'r')
+    except IOError:
+        print('New data file created -> phone_book.txt ')
+        file = open(path, 'w')
+    finally:
+        file.close()
+        
